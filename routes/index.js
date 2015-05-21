b0dc0f0b5e692e63e9a4e24dfc0755c288fa8eb2
Jessica Oberst <joberst@M-10002982.local>
2015-05-20 20:33:26 -0700

    added about route and data.json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+//var appdata = require('../data.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+
 module.exports = router;
+
